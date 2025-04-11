f39872a3102ee27e60f1dddc5c26a6ced69185d7
2acsek <zacs3k@gmail.com>
2025-04-11 17:22:17 +0200

    fix

--- background.js
+++ background.js
@@ -1,7 +1,12 @@
 chrome.runtime.onInstalled.addListener((details) => {
     chrome.contextMenus.create({
-      id: "sendToHost",
-      title: "Send to configured host",
+      id: "sendToPrivate",
+      title: "Forward to private",
+      contexts: ["link"]
+    });
+    chrome.contextMenus.create({
+      id: "sendToTorrents",
+      title: "Forward to torrents",
       contexts: ["link"]
     });
   
@@ -11,11 +16,45 @@ chrome.runtime.onInstalled.addListener((details) => {
   });
   
   chrome.contextMenus.onClicked.addListener((info, tab) => {
-    if (info.menuItemId === "sendToHost") {
+    if (info.menuItemId === "sendToTorrents") {
       chrome.storage.sync.get(["baseUrl"], (data) => {
         const baseUrl = data.baseUrl || "http://localhost:3000";
         const linkUrl = encodeURIComponent(info.linkUrl);
         const fullUrl = `${baseUrl}/download?url=${linkUrl}`;
+        console.log("URLDownloader: URL is being sent to: ", fullUrl)
+        fetch(fullUrl)
+          .then(response => {
+            if (response.ok) {
+              chrome.notifications.create({
+                type: "basic",
+                iconUrl: "icon.png",
+                title: "Success",
+                message: "Link sent successfully!"
+              });
+            } else {
+              chrome.notifications.create({
+                type: "basic",
+                iconUrl: "icon.png",
+                title: "Server Error",
+                message: `Status: ${response.status}`
+              });
+            }
+          })
+          .catch(error => {
+            chrome.notifications.create({
+              type: "basic",
+              iconUrl: "icon.png",
+              title: "Request Failed",
+              message: `Error: ${error.message}`
+            });
+          });
+      });
+    }
+    if(info.menuItemId === "sendToPrivate") {
+      chrome.storage.sync.get(["baseUrl"], (data) => {
+        const baseUrl = data.baseUrl || "http://localhost:3000";
+        const linkUrl = encodeURIComponent(info.linkUrl);
+        const fullUrl = `${baseUrl}/download/private?url=${linkUrl}`;
         console.log("URLDownloader: URL is being sent to: ", fullUrl)
         fetch(fullUrl)
           .then(response => {
